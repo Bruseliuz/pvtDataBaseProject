640148e723e6a303aed7762aa78b2d9db4b927a1
bruseliuz <bruzeliusnils@gmail.com>
2020-04-08 11:24:34 +0200

    added dependencies and sql classes

--- src/main/java/com/example/mysqlspring/MysqlspringApplication.java
+++ src/main/java/com/example/mysqlspring/MysqlspringApplication.java
@@ -2,6 +2,8 @@ package com.example.mysqlspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class MysqlspringApplication extends ServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MysqlspringApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
