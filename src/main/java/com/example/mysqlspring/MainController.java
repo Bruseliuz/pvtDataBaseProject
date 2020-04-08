7386672d2dc12fb93936b511d30ea7747026d46b
bruseliuz <bruzeliusnils@gmail.com>
2020-04-08 12:09:46 +0200

    added dependencies and sql classes

--- src/main/java/com/example/mysqlspring/MainController.java
+++ src/main/java/com/example/mysqlspring/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping(path="/mysqlspring")
 public class MainController {
@@ -12,11 +14,14 @@ public class MainController {
     private UserRepository userRepository;
 
     @PostMapping(path="/add")
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
-
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email, @RequestParam String password) {
+        if (name == null || email == null || password == null) {
+            return "Error in input";
+        }
         User n = new User();
         n.setName(name);
         n.setEmail(email);
+        n.setPassword(password);
         userRepository.save(n);
         return "Saved";
     }
@@ -25,4 +30,9 @@ public class MainController {
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+//    @GetMapping(path="/password")
+//    public @ResponseBody Optional<User> getUserFromPassword() {
+//        return userRepository.findById(1);
+//    }
 }
